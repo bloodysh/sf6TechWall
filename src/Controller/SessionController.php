f61c1c2b7abba0173ac47d9813fd626f27c346a9
Anas Benmsatef <anas.benmsatef@lycee-faure.fr>
2023-05-16 20:40:05 +0200

    Ajout du controller Session + Ajout de la méthode pour checker le nb de connexion

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function mysql_xdevapi\getSession;
+
+class SessionController extends AbstractController
+{
+    #[Route('/session', name: 'session')]
+    public function index(Request $request): Response
+    {
+        $session = $request -> getSession();
+        if ($session ->has('nbVisite')){
+            $nbreVisite = $session->get('nbVisite') + 1;
+
+        }
+        else
+        {
+            $nbreVisite= 1;
+        }
+        $session-> set('nbVisite', $nbreVisite);
+
+        return $this->render('session/index.html.twig', [
+        ]);
+    }
+}
