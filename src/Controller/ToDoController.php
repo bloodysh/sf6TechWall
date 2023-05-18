bd2ebe7d8dd1577f02df77bda874a7abc4c96d9e
_bloodysh <anas.benmsatef@lycee-faure.fr>
2023-05-18 16:50:55 +0200

    to do ajouts

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -19,16 +19,30 @@ class ToDoController extends AbstractController
                 'sagar' => 'exams'
             ];
             $session -> set('todos', $todos);
+            $this -> addFlash('info', "La liste des todos vient d'être initiliasie");
         }
 
         return $this->render('to_do/index.html.twig',
         );
     }
-    #[Route('/todo/{name}/{content}',name: 'todo.add')]
+    #[Route('/to_do/{name}/{content}',name: 'todo.add')]
+
     public function addTodo(Request $request, $name, $content){
+        $session = $request -> getSession();
         if ($session -> has('todos')){
-
+            $todos = $session -> get('todos');
+            if (isset($todos[$name])) {
+                $this -> addFlash('error', 'la liste todo $name existe déjà');
+            }
+            else {
+                $todos [$name] = $content;
+                $this -> addFlash('success', 'le todo d id $name a été ajouté');
+                $session -> set('todos', $todos);
+            }
         }
-
+        else{
+            $this ->addFlash('error', 'la liste des todo n est pas encore initialisée');
+        }
+        return $this-> redirectToRoute('to_do');
     }
 }
