fd7dcae3512f28c8d6b02ce39de41131646e2013
_bloodysh <anas.benmsatef@lycee-faure.fr>
2023-05-18 12:46:32 +0200

    e

---
+++ src/Controller/ToDoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoController extends AbstractController
+{
+    #[Route('/to_do', name: 'to_do')]
+    public function index(Request $request): Response
+    {
+        $session = $request -> getSession();
+        if (!$session -> has('todos')){
+            $todos = [
+                'achat' => 'acheter clé',
+                'sagar' => 'exams'
+            ];
+            $session -> set('todos', $todos);
+        }
+
+        return $this->render('to_do/index.html.twig',
+        );
+    }
+}
